881d11c7db4da2c71ad25823de2115112e6c85b3
AswinVJ666 <aswinvjanakan@gmail.com>
2023-10-31 23:02:48 +0530

    31/10/23

---
+++ practice/code/js1.js
@@ -0,0 +1,14 @@
+console.log('hello world');
+// let name = 'java';
+// console.log(`name is $(name)`);
+let person = {
+    name : 'jhon',
+    age : '40'
+};
+console.log(person);
+person.name = 'wick';
+console.log(person);
+// arrays
+let fruitsName = ['apple','orange','mango'];
+console.log(fruitsName);
+console.log(fruitsName[0]);
---
+++ practice/code/practice1.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("hello programmer")
+        let a = {
+            name:'aswin',age:20
+        }
+        console.log(a)
+        console.log(a.name)
+        console.log(a.age)
